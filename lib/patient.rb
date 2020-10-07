fd3c308c1016fcb3c820fed83f404e4519ffa381
Kristian Joos <kjoos22@gmail.com>
2020-10-07 20:52:28 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,24 @@
+require 'pry'
+class Patient
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end  
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date, doctor)
+    Appointment.new(self, date, doctor)
+  end
+  
+  def appointments
+    binding.pry
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+  
+end
